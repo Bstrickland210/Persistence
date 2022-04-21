daf2301f578c9258a299e5133e6da3820b380ee3
Bridger Strickland <bridger210strick@gmail.com>
2022-04-21 11:41:01 -0600

    Added code

--- Persistence/ViewController.swift
+++ Persistence/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return reds.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath);cell.contentView.backgroundColor = UIColor(displayP3Red: reds[indexPath.row], green: greens[indexPath.row], blue: blues[indexPath.row], alpha: 1)
+                return cell
     }
     
     var reds: [CGFloat] = [0.9]
